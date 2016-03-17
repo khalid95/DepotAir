d554ae450ff63e6ff077cb01af602979b6f34251
khalid <khalid@khalid-PC>
2016-03-17 22:25:23 +0700

    sudah bisa login, dan sudah ada halaman untuk client

--- src/pesanair/LoginScreen.java
+++ src/pesanair/LoginScreen.java
@@ -168,7 +168,34 @@ public class LoginScreen extends javax.swing.JFrame {
             }
 //        System.out.println(""+cek);
             else if(cek == 1){
-                System.out.println("Berhasil "+cek+" "+username.getText()+" "+password.getText());
+                //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+                /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+                * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
+                */
+                try {
+                    for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                        if ("Nimbus".equals(info.getName())) {
+                            javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                            break;
+                        }
+                    }
+                } catch (ClassNotFoundException ex) {
+                    java.util.logging.Logger.getLogger(AdminGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+                } catch (InstantiationException ex) {
+                    java.util.logging.Logger.getLogger(AdminGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+                } catch (IllegalAccessException ex) {
+                    java.util.logging.Logger.getLogger(AdminGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+                } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+                    java.util.logging.Logger.getLogger(AdminGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+                }
+                try {
+                    //</editor-fold>
+                    new ClientGUI(username.getText()).setVisible(true);
+                } catch (Exception ex) {
+                    Logger.getLogger(LoginScreen.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                this.dispose();
+                
             }
             else {
                 System.out.println("Gagal "+cek+" "+username.getText()+" "+password.getText());
